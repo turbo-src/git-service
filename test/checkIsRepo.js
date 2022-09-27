be7f3350e83a81018e1751bfa9e4a9c477c377f7
7db9a <repo.7db9a@gmail.com>
2022-09-27 15:35:50 -0400

    test: pass check is Repo.
    Also create repo.

--- test/checkIsRepo.js
+++ test/checkIsRepo.js
@@ -2,13 +2,15 @@ const assert = require("assert");
 const checkIsRepo = require("../lib/checkIsRepo");
 const cloneRepo = require("../lib/cloneRepo");
 
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 describe("check a repo", function () {
+  this.timeout(3000);
   it("should confirm a repo exists", async function () {
     const resBefore = await checkIsRepo('git-test/turbo-src')
-    //await cloneRepo("git-test/turbo-rsc", "https://github.com/turbo-src/turbo-src")
-    //const resAfter = await checkIsRepo('git-test/turbo-src')
+    await cloneRepo("git-test/turbo-src", "https://github.com/turbo-src/turbo-src")
+    const resAfter = await checkIsRepo('git-test/turbo-src')
     assert.deepEqual(resBefore, { status: 400, state: false }, 'fail confirm git repo')
-    //assert.deepEqual(resAfter, { status: 200, state: true }, 'fail confirm git repo')
+    assert.deepEqual(resAfter, { status: 200, state: true }, 'fail confirm git repo')
   });
   it("should confirm a repo doesn't exists", async function () {
     const res = await checkIsRepo('git-test/not-git')
