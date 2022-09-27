0d2f198808e1f6bdc35d9ad014bd3282c9145297
7db9a <repo.7db9a@gmail.com>
2022-09-27 16:05:50 -0400

    test: add fetchAndPullNewBranch case.

---
+++ test/fetchAndPullNewBranch.js
@@ -0,0 +1,10 @@
+const assert = require("assert");
+const fetchAndPullNewBranch = require("../lib/fetchAndPullNewBranch");
+
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+describe("check a repo", function () {
+  this.timeout(3000);
+  it("should confirm a repo exists", async function () {
+    await fetchAndPullNewBranch("git-test/turbo-src", "https://github.com/turbo-src/turbo-src")
+  });
+});
