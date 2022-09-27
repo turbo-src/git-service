800c37e84d28386fa9467c18bef598bddf554697
7db9a <repo.7db9a@gmail.com>
2022-09-27 14:38:03 -0400

    tests(checkIsRepo): pass two cases.

--- test/checkIsRepo.js
+++ test/checkIsRepo.js
@@ -2,9 +2,9 @@ const assert = require("assert");
 const checkIsRepo = require("../lib/checkIsRepo");
 
 describe("check a repo", function () {
-  it("should confirm a repo exists", async function () {
+  it.only("should confirm a repo exists", async function () {
     const res = await checkIsRepo('git-test/turbo-src')
-    assert.equal(res, { "status": 200, "state": true }, 'fail confirm git repo')
+    assert.deepEqual(res, { status: 200, state: true }, 'fail confirm git repo')
   });
   it("should confirm a repo doesn't exists", async function () {
     const res = await checkIsRepo('git-test/nada')
