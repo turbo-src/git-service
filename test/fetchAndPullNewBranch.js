5188a8c50a8bbdae275edfdb9bdd891b364bd3bc
7db9a <repo.7db9a@gmail.com>
2022-09-27 16:09:51 -0400

    test: does work but not asserted correctly.

--- test/fetchAndPullNewBranch.js
+++ test/fetchAndPullNewBranch.js
@@ -1,10 +1,10 @@
 const assert = require("assert");
 const fetchAndPullNewBranch = require("../lib/fetchAndPullNewBranch");
 
-const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+//const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 describe("check a repo", function () {
-  this.timeout(3000);
+  //this.timeout(3000);
   it("should confirm a repo exists", async function () {
-    await fetchAndPullNewBranch("git-test/turbo-src", "https://github.com/turbo-src/turbo-src")
+    await fetchAndPullNewBranch("git-test/turbo-src", "testDev", "https://github.com/turbo-src/turbo-src")
   });
 });
