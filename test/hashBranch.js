43c01c8253ede2462d8a096010fc60dbf30f8817
7db9a <repo.7db9a@gmail.com>
2022-09-27 19:31:14 -0400

    tests(hashBranch): fail to assert sha256.

--- test/hashBranch.js
+++ test/hashBranch.js
@@ -6,6 +6,8 @@ describe("hash a branch", function () {
   it("should switch to a specified branch and hash", async function () {
     const res = await switchBranch("git-test/turbo-src", "master")
     await hashBranch.tarRepo("git-test/turbo-src")
+    const sha256 = await hashBranch.getSha256("git-test/turbo-src")
     assert.equal(res, 200, 'fail confirm git repo cloned')
+    assert.equal(sha256, "xyz", 'fail to get sha256 of rep')
   });
 });
