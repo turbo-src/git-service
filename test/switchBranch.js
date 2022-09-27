d13532db08683c89f59871cb96e991f424cf70d4
7db9a <repo.7db9a@gmail.com>
2022-09-27 16:29:13 -0400

    tests(switchBranch): pass and confirmed.

--- test/switchBranch.js
+++ test/switchBranch.js
@@ -2,8 +2,10 @@ const assert = require("assert");
 const switchBranch = require("../lib/switchBranch");
 
 describe("switch branch in a specified repo", function () {
-  it.only("should switch branch in a specified repo", async function () {
+  it("should switch branch in a specified repo", async function () {
     const res = await switchBranch("git-test/turbo-src", "master")
-    assert.equal(true, true, 'fail confirm git repo cloned')
+    const resNoBranch = await switchBranch("git-test/turbo-src", "nada")
+    assert.equal(res, 200, 'fail confirm git repo cloned')
+    assert.equal(resNoBranch, 400, 'fail confirm git repo cloned')
   });
 });
