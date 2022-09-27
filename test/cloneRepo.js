feadefb2ce8b44cb88e5374aac42cccb8ea40625
7db9a <repo.7db9a@gmail.com>
2022-09-27 15:12:21 -0400

    Saving progress: clone repo.

--- test/cloneRepo.js
+++ test/cloneRepo.js
@@ -3,7 +3,7 @@ const cloneRepo = require("../lib/cloneRepo");
 
 describe("clone a repo", function () {
   it.only("should confirm repo was cloned", async function () {
-    const res = await cloneRepo("turborsc-service", "https://github.com/turbo-src/service')
-    assert.equesl(true, false, 'fail confirm git repo cloned')
+    const res = await cloneRepo("git-test/turbo-rsc", "https://github.com/turbo-src/turbo-src")
+    assert.equal(true, false, 'fail confirm git repo cloned')
   });
 });
