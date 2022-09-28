0659c5c0268c6ec9a6f542e80960f335e4c09f00
7db9a <repo.7db9a@gmail.com>
2022-09-27 20:17:56 -0400

    test(switchBranch): only pass repoID into args.

--- test/switchBranch.js
+++ test/switchBranch.js
@@ -3,8 +3,8 @@ const switchBranch = require("../lib/switchBranch");
 
 describe("switch branch in a specified repo", function () {
   it("should switch branch in a specified repo", async function () {
-    const res = await switchBranch("git-test/turbo-src", "master")
-    const resNoBranch = await switchBranch("git-test/turbo-src", "nada")
+    const res = await switchBranch("turbo-src", "master")
+    const resNoBranch = await switchBranch("turbo-src", "nada")
     assert.equal(res, 200, 'fail confirm git repo cloned')
     assert.equal(resNoBranch, 400, 'fail confirm git repo cloned')
   });
