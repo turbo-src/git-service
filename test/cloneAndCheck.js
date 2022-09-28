e5708da9bfc0326c0335e9802f20f367ba2b7080
7db9a <repo.7db9a@gmail.com>
2022-09-27 20:11:08 -0400

    test(cloneAndCheck): only pass repoID into args.

--- test/cloneAndCheck.js
+++ test/cloneAndCheck.js
@@ -6,18 +6,18 @@ const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 describe("check a repo", function () {
   this.timeout(3000);
   it("should confirm a repo exists", async function () {
-    const resBefore = await checkIsRepo('git-test/turbo-src')
-    await cloneRepo("git-test/turbo-src", "https://github.com/turbo-src/turbo-src")
-    const resAfter = await checkIsRepo('git-test/turbo-src')
+    const resBefore = await checkIsRepo('turbo-src')
+    await cloneRepo("turbo-src", "https://github.com/turbo-src/turbo-src")
+    const resAfter = await checkIsRepo('turbo-src')
     assert.deepEqual(resBefore, { status: 400, state: false }, 'fail confirm git repo')
     assert.deepEqual(resAfter, { status: 200, state: true }, 'fail confirm git repo')
   });
   it("should confirm a repo doesn't exists", async function () {
-    const res = await checkIsRepo('git-test/not-git')
+    const res = await checkIsRepo('not-git')
     assert.deepEqual(res, { "status": 200, "state": false }, "fail confirm git repo doesn't exist")
   });
   it("should confirm a dir doesn't exists", async function () {
-    const res = await checkIsRepo('git-test/nada')
+    const res = await checkIsRepo('nada')
     assert.deepEqual(res, { "status": 400, "state": false }, "fail confirm git repo doesn't exist")
   });
 });
