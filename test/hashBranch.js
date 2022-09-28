210a535e76a4b666edf57e9bb9b79d6b21c1f53b
7db9a <repo.7db9a@gmail.com>
2022-09-27 22:17:01 -0400

    test(hashBranch): use new all-in-one function of hashBranch.

--- test/hashBranch.js
+++ test/hashBranch.js
@@ -1,30 +1,16 @@
 const assert = require("assert");
 const { v4: uuidv4 } = require('uuid');
-const switchBranch = require("../lib/switchBranch");
 const hashBranch = require("../lib/hashBranch");
-const checkIsRepo = require("../lib/checkIsRepo");
-const cloneRepo = require("../lib/cloneRepo");
-const fetchAndPullNewBranch = require("../lib/fetchAndPullNewBranch");
 
 describe("hash a branch", function () {
   it("should clone repo if doesn't exist, fetch and pull branches, and hash", async function () {
-    let good = false
     const branch = "master"
     const repoID = "turbo-src"
+    const tarName = "turbo-src"
     const remoteURL = "https://github.com/turbo-src/turbo-src"
     
-    const resRepo = await checkIsRepo(repoID)
-    if (resRepo.status === 400) {
-      await cloneRepo(repoID, remoteURL)
-      good = true
-    } else if (resRepo === { status: 200, state: false }) {
-      good = false
-    } else {
-      await fetchAndPullNewBranch(repoID, branch, remoteURL)
-      good = true
-    }
-    const resHashBranch = await hashBranch.tarAndGetHash(repoID, branch, repoID)
-    assert.equal(good, true, "There is a dir with repoID name, but it doesn't have a git work tree.")
+    const resHashBranch = await hashBranch.getDefaultHashBranch(repoID, remoteURL, branch, tarName)
+
     assert.deepEqual(
       resHashBranch,
       {
