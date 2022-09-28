a608ab9724c14fbea5ca92001ee93c5d4fb25715
7db9a <repo.7db9a@gmail.com>
2022-09-27 20:35:34 -0400

    tests(hasBranch): pass repoID and tarName as args.
    Previous commit was intended for lib/ not test/.

--- test/hashBranch.js
+++ test/hashBranch.js
@@ -1,16 +1,18 @@
 const assert = require("assert");
+const { v4: uuidv4 } = require('uuid');
 const switchBranch = require("../lib/switchBranch");
 const hashBranch = require("../lib/hashBranch");
 
 describe("hash a branch", function () {
   it("should switch to a specified branch and hash", async function () {
-    const res = await switchBranch("git-test/turbo-src", "master")
-    await hashBranch.tarRepo("git-test/turbo-src")
-    const sha256 = await hashBranch.getSha256("git-test/turbo-src")
+    const res = await switchBranch("turbo-src", "master")
+    const id = uuidv4()
+    await hashBranch.tarRepo("turbo-src", `turbo-src-${id}`)
+    const sha256 = await hashBranch.getSha256(`turbo-src-${id}`)
     assert.equal(res, 200, 'fail confirm git repo cloned')
     assert.equal(
 	    sha256,
-	    '627107bceae7c0207976a53d0208a39d359404ea6e5c177522ae7d354e1bbee7',
+	    'f38fc74c622390aadb35b87519629e6601d6384b764ec52ef94fc859c1e5e3e3',
 	    'fail to get sha256 of rep'
     )
   });
