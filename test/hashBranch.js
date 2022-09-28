5d650dee8fdf0075cffb4f95c9b18f1fe4f40b0a
7db9a <repo.7db9a@gmail.com>
2022-09-27 21:38:34 -0400

    test(hashBranch): modify to accomodate single function to do it all.

--- test/hashBranch.js
+++ test/hashBranch.js
@@ -5,15 +5,14 @@ const hashBranch = require("../lib/hashBranch");
 
 describe("hash a branch", function () {
   it("should switch to a specified branch and hash", async function () {
-    const res = await switchBranch("turbo-src", "master")
-    const id = uuidv4()
-    await hashBranch.tarRepo("turbo-src", `turbo-src-${id}`)
-    const sha256 = await hashBranch.getSha256(`turbo-src-${id}`)
-    assert.equal(res, 200, 'fail confirm git repo cloned')
-    assert.equal(
-	    sha256,
-	    'f38fc74c622390aadb35b87519629e6601d6384b764ec52ef94fc859c1e5e3e3',
-	    'fail to get sha256 of rep'
+    const res = await hashBranch.tarAndGetHash("turbo-src", "master", "turbo-src")
+    assert.deepEqual(
+      res,
+      {
+        status: 200,
+        defaultHash: 'f38fc74c622390aadb35b87519629e6601d6384b764ec52ef94fc859c1e5e3e3'
+      },
+      'fail to get hash of branch'
     )
   });
 });
