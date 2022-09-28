52b6af18d2d45a6bd0e98023e1bf694f97e5bb54
7db9a <repo.7db9a@gmail.com>
2022-09-27 22:20:32 -0400

    tests(hashBranch): don't pass tarName.

--- test/hashBranch.js
+++ test/hashBranch.js
@@ -6,10 +6,9 @@ describe("hash a branch", function () {
   it("should clone repo if doesn't exist, fetch and pull branches, and hash", async function () {
     const branch = "master"
     const repoID = "turbo-src"
-    const tarName = "turbo-src"
     const remoteURL = "https://github.com/turbo-src/turbo-src"
     
-    const resHashBranch = await hashBranch.getDefaultHashBranch(repoID, remoteURL, branch, tarName)
+    const resHashBranch = await hashBranch.getDefaultHashBranch(repoID, remoteURL, branch)
 
     assert.deepEqual(
       resHashBranch,
