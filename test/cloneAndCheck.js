73a81d71cc2950427e8f1af7f13ea60c45e57932
7db9a <>
2024-01-17 22:18:22 -0500

    fix: clone repo.

--- test/cloneAndCheck.js
+++ test/cloneAndCheck.js
@@ -7,7 +7,7 @@ const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 describe("check a repo", function () {
   this.timeout(3000);
   it("should confirm a repo exists", async function () {
-    const remoteURL = "https://github.com/turbo-src/turbo-src"
+    const remoteURL = "https://github.com/turbo-service"
     const remoteHashID = crypto.SHA256(remoteURL).toString(crypto.enc.Hex)
     const resBefore = await checkIsRepo(remoteHashID)
     await cloneRepo(remoteHashID, remoteURL)
--- test/cloneRepo.js
+++ test/cloneRepo.js
@@ -1,9 +1,22 @@
 const assert = require("assert");
-const cloneRepo = require("../lib/switchBranch");
+const crypto = require('crypto-js')
+const cloneRepo = require("../lib/cloneRepo");
+const checkIsRepo = require("../lib/checkIsRepo");
 
-describe("switch branch in a specified repo", function () {
-  it.only("should switch branch in a specified repo", async function () {
-    const res = await switchBranch("git-test/turbo-tsc", "testDev")
-    assert.equal(true, true, 'fail confirm git repo cloned')
+describe("Clone a repo", function () {
+  this.timeout(10_000);
+  it.only("should clone a repo repo", async function () {
+    const remoteURL = "https://github.com/turbo-src/git-service"
+    const remoteHashID = crypto.SHA256(remoteURL).toString(crypto.enc.Hex)
+    res = await cloneRepo(remoteHashID, remoteURL)
+    console.log('clone res', res)
+    const repoExists = await checkIsRepo(remoteHashID)
+    assert.equal(
+      remoteHashID,
+      "66806c53dad8d713eef555fb85ef608def74b75b05eb0e54f6f9e38eec3e54b8",
+      "failed to get a reprodicible hash ID"
+    )
+    assert.deepEqual(repoExists, { status: 200, state: true }, 'fail confirm git repo')
+
   });
 });
