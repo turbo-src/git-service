b72c3832ee02028706601288a9f8b40c11a18347
7db9a <>
2024-01-20 03:47:38 -0500

    tests(giteTokenCreator): gitea api token field in turbosrc.config overwritten.
    Only changes within the container, not on host.

--- test/giteaTokenCreator.js
+++ test/giteaTokenCreator.js
@@ -1,11 +1,22 @@
 const assert = require("assert");
+const fs = require('fs');
 const GiteaTokenCreator = require("../lib/giteaTokenCreator");
+const JsonKeyValueStore = require("../lib/jsonValueStore");
 
 describe("Create a user", function () {
   this.timeout(10_000);
-  it("should create a user on gitea", async function () {
-    const username = 'username';
-    const password = 'password';
+
+  const jsonFilePath = "./turbosrc.config";
+  let store
+  before(async () => {
+    store = new JsonKeyValueStore(jsonFilePath);
+    assert(fs.existsSync(jsonFilePath), "config file should be created");
+  });
+  it("should create an access token for gitea admin user", async function () {
+    const username = store.getValue('GithubName');
+    const password = store.getValue('GithubPassword');
+    let token
+
     const giteaTokenCreator = new GiteaTokenCreator('http://gitea:3000/api/v1', username, password);
 
     const tokenDetails = {
@@ -19,10 +30,18 @@ describe("Create a user", function () {
     };
 
     try {
-      const token = await giteaTokenCreator.createAccessToken(username, tokenDetails);
+      token = await giteaTokenCreator.createAccessToken(username, tokenDetails);
       console.log('Access token created:', token);
     } catch (error) {
       console.error('Failed to create access token:', error);
     }
+
+    store.setValue('GithubApiToken', token['sha1']);
+
+    assert.equal(
+      store.getValue('GithubApiToken'),
+      token['sha1'],
+      'fail to set config with new acess token'
+    )
   })
 });
